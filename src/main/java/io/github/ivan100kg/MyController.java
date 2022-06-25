f7f10f4a7e1a0a39b8a5b5974442a9607f247d8d
Ivan B <ivar110kg@gmail.com>
2022-06-25 09:50:30 +0400

    Add file MyController.java

---
+++ src/main/java/io/github/ivan100kg/MyController.java
@@ -0,0 +1,13 @@
+package io.github.ivan100kg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
+}
