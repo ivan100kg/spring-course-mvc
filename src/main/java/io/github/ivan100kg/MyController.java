9d4b4c582ad96b5dfd3dea222ce141d72e70c029
Ivan B <ivar110kg@gmail.com>
2022-06-26 20:39:45 +0400

    Update MyController.java, use Model

--- src/main/java/io/github/ivan100kg/MyController.java
+++ src/main/java/io/github/ivan100kg/MyController.java
@@ -1,8 +1,11 @@
 package io.github.ivan100kg;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -17,8 +20,16 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showDetails() {
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showDetails() {
+    public String showDetails(HttpServletRequest request, Model model) {
+        String empName = request.getParameter("employeeName");
+        empName = "Mister " + empName;
+        model.addAttribute("nameAttr", empName);
         return "show-emp-details-view";
     }
 }
